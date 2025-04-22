742537f187abcfd3e6a112f9519a324ad36e76a9
Thulasi Venu <103374793+Thulasivenu@users.noreply.github.com>
2025-04-22 11:08:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import IncrementCount from './Components/IncrementCount'
+import TextString from './Components/TextString'
+import Liked from './Components/Liked'
+import TwoVariable from './Components/TwoVariable'
+import LoginForm from './Components/Login'
+import AnimatedText from './Components/AnimatedText'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      {/* <IncrementCount/> */}
+      {/* <TextString/> */}
+      {/* <Liked/> */}
+      {/* <TwoVariable/> */}
+      {/* <LoginForm/> */}
+     <AnimatedText/>
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+      
+      
+      
+      
+    </>
+  )
+}
+
+export default App
